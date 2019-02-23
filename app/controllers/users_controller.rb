b7bf91b40fc9664812460273517cd2cdd28b8789
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-02-23 14:59:23 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,7 @@ class UsersController < ApplicationController
 
   def show
     if user_signed_in?
-      byebug
       @user = authorize User.find_by(current_user.id)
-      if @user.nil?
-        flash[:alert] = "Access denied."
-        redirect_to root_path
-      end
-    else
-      flash[:alert] = "Access denied."
-      redirect_to root_path
     end
   end
 end
